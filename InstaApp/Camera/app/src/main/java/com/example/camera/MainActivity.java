8a78f9061a942c94e12a4c4192fd2ccf43d0fb61
Your Name <you@example.com>
2023-04-14 12:46:34 +0200

    Photo taking &Video recording --final

--- InstaApp/Camera/app/src/main/java/com/example/camera/MainActivity.java
+++ InstaApp/Camera/app/src/main/java/com/example/camera/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.camera;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
+import androidx.camera.core.VideoCapture;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -33,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ImageCapture imageCapture;
+    private VideoCapture videoCapture;
 
+    @SuppressLint("RestrictedApi")
     private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
 
         Preview preview = new Preview.Builder().build();
@@ -44,13 +51,17 @@ public class MainActivity extends AppCompatActivity {
                 .setTargetRotation(binding.previewView.getDisplay().getRotation())
                 .build();
 
+        videoCapture = new VideoCapture.Builder()
+                .setTargetRotation(binding.previewView.getDisplay().getRotation())
+                .build();
 
         CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+//                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
                 .build();
 
 
-        cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture, preview);
+        cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture, videoCapture, preview);
     }
 
     private void cameraInit() {
@@ -65,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         }, ContextCompat.getMainExecutor(this));
     }
 
+    @SuppressLint({"RestrictedApi", "MissingPermission"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,33 +90,85 @@ public class MainActivity extends AppCompatActivity {
         }
 
         binding.takePhoto.setOnClickListener(view -> {
-            File mImageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "photos");
+            File mImageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "KGphotos");
             boolean isDirectoryCreated = mImageDir.exists() || mImageDir.mkdirs();
+            String fileName = new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss").format(new Date());
             File file;
             if (isDirectoryCreated) {
-               file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/photos/", "x.jpg");
-            }
-            else return;
+                file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/KGphotos/", fileName + ".jpg");
+            } else return;
             ContentValues contentValues = new ContentValues();
-            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME,new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss").format(new Date()));
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME,fileName);
             contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
-//            ImageCapture.OutputFileOptions outputFileOptions =
-//                    new ImageCapture.OutputFileOptions.Builder(
-//                            getContentResolver(),
-//                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-//                            contentValues)
-//                            .build();
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES+"/KGphotos");
             ImageCapture.OutputFileOptions outputFileOptions =
-                    new ImageCapture.OutputFileOptions.Builder(file).build();
+                    new ImageCapture.OutputFileOptions.Builder(
+                            getContentResolver(),
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            contentValues)
+                            .build();
+//            ImageCapture.OutputFileOptions outputFileOptions =
+//                    new ImageCapture.OutputFileOptions.Builder(file).build();
             imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this),
                     new ImageCapture.OnImageSavedCallback() {
                         @Override
                         public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                            // photo saved!!!
+                            Toast.makeText(MainActivity.this, "Photo saved", Toast.LENGTH_SHORT).show();
                         }
 
                         @Override
                         public void onError(@NonNull ImageCaptureException exception) {
+                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        });
+        binding.takeVideo.setOnClickListener(view -> {
+            if (binding.takeVideo.getText() == "Stop recording") {
+                videoCapture.stopRecording();
+                binding.takeVideo.setText("Record video");
+                return;
+
+            }
+            binding.takeVideo.setText("Stop recording");
+            File mImageDir = new File(getExternalFilesDir(Environment.DIRECTORY_MOVIES), "KGvideos");
+            boolean isDirectoryCreated = mImageDir.exists() || mImageDir.mkdirs();
+            String fileName = new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss").format(new Date());
+            File file;
+            if (isDirectoryCreated) {
+                file = new File(getExternalFilesDir(Environment.DIRECTORY_MOVIES) + "/KGvideos/", fileName + ".mp4");
+            } else return;
+            VideoCapture.OutputFileOptions outputFileOptions =
+                    new VideoCapture.OutputFileOptions.Builder(file).build();
+//            videoCapture.startRecording(outputFileOptions, ContextCompat.getMainExecutor(this), new VideoCapture.OnVideoSavedCallback() {
+//                @Override
+//                public void onVideoSaved(@NonNull VideoCaptu
+//                }
+//            });
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH,(Environment.DIRECTORY_MOVIES) + "/KGvideos");
+
+
+            videoCapture.startRecording(
+                    new VideoCapture.OutputFileOptions.Builder(
+                            getContentResolver(),
+                            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                            contentValues
+                    ).build(),
+//                    outputFileOptions,
+                    ContextCompat.getMainExecutor(this),
+                    new VideoCapture.OnVideoSavedCallback() {
+                        @Override
+                        public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
+                            Toast.makeText(MainActivity.this,"video saved",Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onError(int videoCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                            Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
+                            // error
                         }
                     });
 
@@ -123,7 +187,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             if (!allGranted) {
                 Toast.makeText(MainActivity.this, "Error: Permissions not granted", Toast.LENGTH_LONG).show();
-
+            }else{
+                cameraInit();
             }
 
         }
